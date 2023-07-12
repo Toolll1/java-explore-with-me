0468ac41dd783109b95e3d52b65313e78068f380
Денис <d-e-n-i-s-ne@yandex.ru>
2023-07-12 14:20:21 +0300

    новые тесты на GitHub для сервиса статистики

--- stats-module/stats-server/src/main/java/ru/practicum/controller/StatsController.java
+++ stats-module/stats-server/src/main/java/ru/practicum/controller/StatsController.java
@@ -2,6 +2,7 @@ package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.model.HitDto;
 import ru.practicum.model.StatsDto;
@@ -18,6 +19,7 @@ public class StatsController {
     private final StatsService service;
 
     @PostMapping("/hit")
+    @ResponseStatus(HttpStatus.CREATED)
     public HitDto create(@Valid @RequestBody HitDto hit) {
 
         return service.create(hit);
--- stats-module/stats-server/src/main/java/ru/practicum/service/StatsService.java
+++ stats-module/stats-server/src/main/java/ru/practicum/service/StatsService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.exceptions.BadRequestException;
 import ru.practicum.model.Hit;
 import ru.practicum.model.HitDto;
 import ru.practicum.model.HitMapper;
@@ -34,7 +35,7 @@ public class StatsService {
     public List<StatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
 
         if (start.isAfter(end)) {
-            throw new IllegalArgumentException("Invalid time interval");
+            throw new BadRequestException("Invalid time interval");
         }
 
         if (uris == null || uris.isEmpty()) {
